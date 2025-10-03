bdcac105d031790d0a4e99d86aad7a3889c6a4e4
yosef <yosefpetrosh@gmail.com>
2025-10-03 17:38:57 -0500

    Implement CI/CD pipelines and GitHub actions

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import helmet from "helmet";
 import authRoutes from "#routes/auth.routes.js";
 import securityMiddleware from "#middleware/security.middleware.js"
 import usersRoutes from "#routes/users.routes.js";
-import apiRouter from '#routes/api.route.js';
+import apiRouter from '#routes/api.routes.js';
 const app = express();
 app.use('/api', apiRouter);
 const isProd = process.env.NODE_ENV === "production";
@@ -58,4 +58,7 @@ app.get('/api', (req, res) => {
 });
 app.use('/api/auth', authRoutes)
 app.use('/api/users', usersRoutes);
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Route not found" });
+})
 export default app;
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,6 +1,6 @@
 // src/routes/api.route.js
 import { Router } from 'express';
-import usersRouter from '#routes/users.route.js';
+import usersRouter from '#routes/users.routes.js';
 
 const router = Router();
 
